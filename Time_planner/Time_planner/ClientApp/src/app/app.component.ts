b082d582ebf2ec9f8b5b2283f8e5b2e89b36266d
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2020-10-11 17:15:37 +0200

    get user localization

--- Time_planner/Time_planner/ClientApp/src/app/app.component.ts
+++ Time_planner/Time_planner/ClientApp/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => { this.lat = position.coords.latitude; this.lng = position.coords.longitude; });
+    }
+    else { // "Geolocation is not supported by this browser."
+    }
+  }
 }
--- Time_planner/Time_planner/ClientApp/src/app/app.module.ts
+++ Time_planner/Time_planner/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +22,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyABcru_NMTTmJLKCoddjaFgqeb9K3X4P48'
+    }),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
