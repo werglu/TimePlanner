d1efb2f87bf69020f64e76a7ebdd3d7c5012a4ef
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2020-10-16 00:19:07 +0200

    add tests projects

---
+++ Time_planner/UITests/UITests.cs
@@ -0,0 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace UITests
+{
+    public class UITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        public UITests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Can_Navigate_To_MainPage()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44396/");
+            Assert.Equal("Time planner app", _driver.Title);
+        }
+    }
+}
