92ce984a54dd8de0b3037f9f94e80f6f07b4eb68
Marta Banel <01130552@pw.edu.pl>
2020-10-18 01:46:41 +0200

    Add path to map

--- Time_planner/Time_planner/ClientApp/src/app/home/home.component.ts
+++ Time_planner/Time_planner/ClientApp/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, AfterViewInit } from '@angular/core';
 export class HomeComponent implements AfterViewInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
+  paths: { lat: number, lng: number } [] = [];
 
   constructor() {
   }
@@ -17,9 +18,19 @@ export class HomeComponent implements AfterViewInit {
 
   getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => { this.lat = position.coords.latitude; this.lng = position.coords.longitude; });
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.paths = [
+          { lat: this.lat, lng: this.lng },
+          { lat: this.lat + 0.02, lng: this.lng + 0.02 },
+          { lat: this.lat + 0.01, lng: this.lng + 0.03 },
+          { lat: this.lat - 0.01, lng: this.lng + 0.01 },
+        ];
+      });
     }
-    else { // "Geolocation is not supported by this browser."
+    else {
+      console.warn("Geolocation is not supported by this browser")
     }
   }
 }
