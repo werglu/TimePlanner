7c3159ef422f77d71ef543fd3a1d387075b05188
Anna Szmurło <01130615@pw.edu.pl>
2020-11-14 00:47:49 +0100

    add auth guard to deny access

--- Time_planner/Time_planner/ClientApp/src/app/app.module.ts
+++ Time_planner/Time_planner/ClientApp/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { NotificationDetailsModalComponent } from './notifications/notification-details-modal/notification-details-modal.component';
 import { EditTaskModalComponent } from './to-do-list/edit-task-modal/edit-task-modal.component';
 import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
@@ -65,14 +66,14 @@ import { AccessDeniedComponent } from './access-denied/access-denied.component';
     }),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'calendar', component: CalendarComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'to-do-list', component: ToDoListComponent },
+      { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
+      { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
+      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
+      { path: 'to-do-list', component: ToDoListComponent, canActivate: [AuthGuard] },
       { path: 'access-denied', component: AccessDeniedComponent },
     ])
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
