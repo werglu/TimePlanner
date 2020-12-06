2cbb59967fe1ef9540461f7ed5ae19dba37d15f6
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2020-12-07 00:24:24 +0100

    add dates constraint when edit task

--- Time_planner/Time_planner/ClientApp/src/app/to-do-list/edit-task-modal/edit-task-modal.component.ts
+++ Time_planner/Time_planner/ClientApp/src/app/to-do-list/edit-task-modal/edit-task-modal.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { ListCategory } from '../listCategory';
 import { ListCategoriesService } from '../listCategories.service';
 import { Task } from '../task';
 import { TasksService } from '../tasks.service';
-import { CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'edit-task-modal',
@@ -16,11 +15,23 @@ export class EditTaskModalComponent implements OnInit {
   editTaskForm: FormGroup;
   public listCategories: Array<ListCategory> = [];
   public priorities: Array<string> = ['High priority', 'Medium priority', 'Low priority'];
+  public splits: Array<number> = [1, 2, 3, 4, 5, 6];
+  public hours: Array<number> = Array.from({ length: 51 }, (v, k) => k)
+  public minutes: Array<number> = [0, 5, 15, 30, 45]
+  checkedDays: Array<boolean> = [true, false, false, false, false, false, false]
   currentCategory: ListCategory;
   currentTask: Task;
   isDone = false;
   addDatesOff = true;
   choosenPriority = 1;
+  choosenSplit = 1;
+  choosenHour = 1;
+  choosenMinute = 0;
+  choosenHourIndex = 1;
+  choosenMinuteIndex = 0;
+  choosenDays = 1 + 2 + 4 + 8 + 16;
+
+  public addDateConstraintsOff = true;
   @Input() userId: string;
   @Input() editedTask: Task;
   @Output() onCancel = new EventEmitter();
@@ -48,8 +59,34 @@ export class EditTaskModalComponent implements OnInit {
       this.tasksService.getTask(Number(this.editedTask.id)).subscribe((task) => {
         this.currentTask = task;
         this.currentCategory = (this.listCategories.filter(c => c.id == task.categoryId))[0];
+        if (task.time != null) {
+          this.turnOnAddDateConstraints();
+          this.choosenHour = Math.floor(task.time / 60);
+          this.choosenHourIndex = this.choosenHour;
+          this.choosenMinute = task.time - (60 * this.choosenHour);
+          this.choosenMinuteIndex = this.minutes.findIndex(m => m == this.choosenMinute);
+          this.choosenSplit = task.split;
+          this.getSelectedDays(this.currentTask.days);
+        }
       });
     }
+  }
+
+  turnOnAddDates() {
+    this.addDatesOff = false;
+  }
+
+  turnOffAddDates() {
+    this.addDatesOff = true;
+  }
+
+  turnOnAddDateConstraints() {
+    this.addDateConstraintsOff = false;
+    this.splits = [1, 2, 3, 4, 5];
+  }
+
+  turnOffAddDateConstraints() {
+    this.addDateConstraintsOff = true;
   }
 
   cancel() {
@@ -58,6 +95,72 @@ export class EditTaskModalComponent implements OnInit {
 
   checked(isDone: boolean) {
     this.isDone = isDone;
+  }
+
+  getSelectedDays(days: number) {
+    this.choosenDays = days;
+    this.checkedDays[0] = false; //update default value
+
+    var count = 0;
+    if (days >= 64) {
+      this.checkedDays[6] = true;
+      days -= 64;
+      count++;
+    }
+    if (days >= 32) {
+      this.checkedDays[65] = true;
+      days -= 32; count++;
+    }
+    if (days >= 16) {
+      this.checkedDays[4] = true;
+      days -= 16; count++;
+    }
+    if (days >= 8) {
+      this.checkedDays[3] = true;
+      days -= 8; count++;
+    }
+    if (days >= 4) {
+      this.checkedDays[2] = true;
+      days -= 4; count++;
+    }
+    if (days >= 2) {
+      this.checkedDays[1] = true;
+      days -= 2; count++;
+    }
+    if (days >= 1) {
+      this.checkedDays[0] = true;
+      days -= 1; count++;
+    }
+
+    this.splits = Array.from({ length: count }, (v, k) => k + 1)
+    if (this.choosenSplit > this.splits.length) {
+      this.choosenSplit = 1; 
+    }
+  }
+
+  onDayChange() {
+    var days = 0;
+    var count = 0;
+    var day = 6;
+    var daysOfWeek = ["sun", "sat", "fri", "thu", "wed", "tue", "mon"];
+    daysOfWeek.forEach((dayOfWeek) => {
+      days *= 2;
+      var element = document.getElementById('weekday-' + dayOfWeek) as HTMLInputElement
+      if (element.checked) {
+        this.checkedDays[day] = true;
+        days += 1;
+        count += 1;
+      }
+      else {
+        this.checkedDays[day] = false;
+      }
+      day--;
+    });
+    this.choosenDays = days;
+    this.splits = Array.from({ length: count }, (v, k) => k + 1)
+    if (this.choosenSplit > this.splits.length) {
+      this.choosenSplit = 1;
+    }
   }
 
   getCategories() {
@@ -85,6 +188,18 @@ export class EditTaskModalComponent implements OnInit {
     }
   }
 
+  onSplitChange(split: number) {
+    this.choosenSplit = split;
+  }
+
+  onHourChange(hour: number) {
+    this.choosenHour = hour;
+  }
+
+  onMinuteChange(minute: number) {
+    this.choosenMinute = minute;
+  }
+
   getFormValue(): Task {
     return {
       id: Number(this.editedTask.id),
@@ -93,6 +208,9 @@ export class EditTaskModalComponent implements OnInit {
       isDone: this.isDone,
       title: (<HTMLInputElement>document.getElementById('title')).value,
       priority: this.choosenPriority,
+      split: this.choosenSplit,
+      days: this.choosenDays,
+      time: this.choosenHour * 60 + this.choosenMinute,
       date0: this.editedTask.date0 == null ? null :  new Date(this.editedTask.date0),
       date1: this.editedTask.date1 == null ? null : new Date(this.editedTask.date1),
       date2: this.editedTask.date2 == null ? null : new Date(this.editedTask.date2),
