c53e43e6290ee10dddcbd26a69060ccfda2c3602
Marta Banel <01130552@pw.edu.pl>
2020-12-29 22:06:05 +0100

    Add missing api methods

--- Time_planner_api/Time_planner_api/Controllers/ListCategoriesController.cs
+++ Time_planner_api/Time_planner_api/Controllers/ListCategoriesController.cs
@@ -74,6 +74,77 @@ namespace Time_planner_api.Controllers
             return Ok();
         }
 
+
+        // PUT api/ListCategories/5
+        /// <summary>
+        /// Edit category
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <param name="newCategory">New object of category</param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<ActionResult> PutCategory([FromRoute] int id, ListCategory newCategory)
+        {
+            ListCategory category = _context.ListCategories.Where(p => p.Id == id).Single<ListCategory>();
+
+            if (GetUserId() != category.OwnerId || newCategory.OwnerId != category.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            category.Category = newCategory.Category;
+            category.OwnerId = newCategory.OwnerId;
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(newCategory.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+
+        // DELETE: api/ListCategories/5
+        /// <summary>
+        /// Delete defined category
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <returns>Deleted category</returns>
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult<ListCategory>> DeleteCategory([FromRoute] int id)
+        {
+            var category = await _context.ListCategories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            if (GetUserId() != category.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            _context.ListCategories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
         private bool CategoryExists(int id)
         {
             return _context.ListCategories.Any(e => e.Id == id);
