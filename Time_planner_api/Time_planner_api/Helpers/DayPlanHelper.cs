628878f70c948fd1c5fb3609bdf8e3f08c089985
Marta Banel <01130552@pw.edu.pl>
2021-01-03 22:03:43 +0100

    Fix day planning + measure time travel

--- Time_planner_api/Time_planner_api/Helpers/DayPlanHelper.cs
+++ Time_planner_api/Time_planner_api/Helpers/DayPlanHelper.cs
@@ -7,6 +7,7 @@ namespace Time_planner_api.Helpers
 {
     public class DayPlanHelper
     {
+        private static double Velocity { get { return 1.0; } }
         public static List<CalendarItem> FindShortestRoute(List<Event> eventsToPlan, List<Task> tasksToPlan, DateTime date, double startMinutes, double endMinutes)
         {
             var events = eventsToPlan.Select(x => x).ToList();
@@ -14,8 +15,8 @@ namespace Time_planner_api.Helpers
             var result = new List<CalendarItem>();
             var tasksLeftOut = new List<CalendarItem>();
 
-            tasksLeftOut.AddRange(tasks.Where(x => !x.Longitude.HasValue || !x.Latitude.HasValue).Select(t => new CalendarItem { T = t, Assigned = false }).ToList());
-            tasks.RemoveAll(x => !x.Longitude.HasValue || !x.Latitude.HasValue);
+            tasksLeftOut.AddRange(tasks.Where(x => x.Longitude.GetValueOrDefault(0.0) == 0.0 || x.Latitude.GetValueOrDefault(0.0) == 0.0).Select(t => new CalendarItem { T = t, Assigned = false }).ToList());
+            tasks.RemoveAll(x => x.Longitude.GetValueOrDefault(0.0) == 0.0 || x.Latitude.GetValueOrDefault(0.0) == 0.0);
 
             events.Sort((x, y) =>
             {
@@ -47,6 +48,11 @@ namespace Time_planner_api.Helpers
             }
             if (currentWindowStart.Item1 < dayEnd)
             {
+                if (currentWindowStart.Item2 == double.MaxValue && events.Count > 0)
+                {
+                    var ev = events.Last();
+                    currentWindowStart = (currentWindowStart.Item1, ev.Latitude, ev.Longitude);
+                }
                 result.AddRange(GetTasksBetween(tasks, currentWindowStart, (dayEnd, double.MaxValue, double.MaxValue)));
             }
 
@@ -70,29 +76,44 @@ namespace Time_planner_api.Helpers
             }
 
             var tasksBetween = new List<Task>();
-            var timeLeft = (int)(end.Item1 - start.Item1).TotalMinutes;
+            var totalTime = (int)(end.Item1 - start.Item1).TotalMinutes;
+            var bestTime = 0.0;
+            var bestList = new List<Task>();
             foreach(var task in tasks)
             {
-                var time = GetTime(task);
-                if (time <= timeLeft)
+                if (bestTime + GetTime(task) > totalTime)
                 {
-                    tasksBetween.Add(task);
-                    timeLeft -= time;
+                    continue;
                 }
-                if (tasksBetween.Count == SalesmanHelper.MaxTaskCount)
+                tasksBetween.Add(task);
+                tasksBetween = SalesmanHelper.FindShortestRoute(tasksBetween);
+                var newResult1 = FindBestOrder(tasksBetween, start, end);
+                tasksBetween.Reverse();
+                var newResult2 = FindBestOrder(tasksBetween, start, end);
+                var newResult = newResult1.Item2 < newResult2.Item2 ? newResult1 : newResult2;
+                var time = newResult.Item2;
+                if (time > totalTime)
                 {
-                    break;
+                    tasksBetween.Remove(task);
+                }
+                else
+                {
+                    bestList = newResult.Item1;
+                    bestTime = time;
+                    if (tasksBetween.Count == SalesmanHelper.MaxTaskCount)
+                    {
+                        break;
+                    }
                 }
             }
 
-            if (tasksBetween.Count == 0)
-            {
-                return new List<CalendarItem>();
-            }
+            tasks.RemoveAll(t => bestList.Any(tt => tt.Id == t.Id));
 
-            tasks.RemoveAll(t => tasksBetween.Any(tt => tt.Id == t.Id));
+            return bestList.Select(x => new CalendarItem { T = x, Assigned = true }).ToList();
+        }
 
-            tasksBetween = SalesmanHelper.FindShortestRoute(tasksBetween);
+        private static (List<Task>, double) FindBestOrder(List<Task> tasksBetween, (DateTime, double, double) start, (DateTime, double, double) end)
+        {
             var bestDistance = double.MaxValue;
             int bestIndex = 0;
             int index = 0;
@@ -117,17 +138,38 @@ namespace Time_planner_api.Helpers
                 prevTask = task;
             }
 
-            var result = new List<CalendarItem>();
-            result.AddRange(tasksBetween.GetRange(bestIndex, tasksBetween.Count - bestIndex).Select(x => new CalendarItem { T = x, Assigned = true }).ToList());
+            var result = new List<Task>();
+            result.AddRange(tasksBetween.GetRange(bestIndex, tasksBetween.Count - bestIndex).ToList());
             if (bestIndex > 0)
             {
-                result.AddRange(tasksBetween.GetRange(0, bestIndex).Select(x => new CalendarItem { T = x, Assigned = true }).ToList());
+                result.AddRange(tasksBetween.GetRange(0, bestIndex).ToList());
             }
-            return result;
+            return (result, CalculateTime(result, start, end));
+        }
+
+        private static double CalculateTime(List<Task> tasks, (DateTime, double, double) start, (DateTime, double, double) end)
+        {
+            if (tasks.Count == 0)
+            {
+                return 0;
+            }
+            var lastPoint = (start.Item2, start.Item3);
+            var distance = 0.0;
+            foreach(var task in tasks)
+            {
+                distance += GetDistance(task, lastPoint);
+                lastPoint = (task.Latitude.Value, task.Longitude.Value);
+            }
+            distance += GetDistance(tasks.Last(), (end.Item2, end.Item3));
+            return tasks.Sum(x => GetTime(x)) + distance * Velocity;
         }
 
         private static double GetDistance(Task task, (double, double) point)
         {
+            if (point.Item1 == double.MaxValue)
+            {
+                return 0.0;
+            }
             return (task.Latitude.Value - point.Item1) * (task.Latitude.Value - point.Item1) + (task.Longitude.Value - point.Item2) * (task.Longitude.Value - point.Item2);
         }
 
